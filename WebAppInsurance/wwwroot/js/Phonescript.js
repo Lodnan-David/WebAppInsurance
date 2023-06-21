9c9e9bdcd87f3a94c1add84c33cd545f57ff88c7
Lodnan-David <135867605+Lodnan-David@users.noreply.github.com>
2023-06-21 11:37:43 +0200

    final

---
+++ WebAppInsurance/wwwroot/js/Phonescript.js
@@ -0,0 +1,10 @@
+﻿function restrictInputToNumbers(input) {
+    input.value = input.value.replace(/\D/g, '');
+}
+// Validace pole vstupu pro mobilní číslo
+const PhoneInput = document.getElementById('Phone-input');
+PhoneInput.addEventListener('input', function (event) {
+    const value = event.target.value;
+    const isValid = /^\+?[0-9]+$/.test(value);
+    event.target.setCustomValidity(isValid ? '' : 'Prosím, zadejte platné mobilní číslo.');
+});
